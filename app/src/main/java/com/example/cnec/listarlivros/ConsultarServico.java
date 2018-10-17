283da9f7c30cffa37c89a96bee3e1f76177b2f08
symphxnys <dammitbruna@gmail.com>
2018-10-16 21:45:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cnec/listarlivros/ConsultarServico.java
@@ -0,0 +1,78 @@
+package com.example.cnec.listarlivros;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by CNEC on 02/10/2018.
+ */
+
+public class ConsultarServico extends AsyncTask<String, Void, String> {
+
+    private MainActivity ma;
+
+    public ConsultarServico(MainActivity ma) {
+        this.ma = ma;
+    }
+
+
+    @Override
+    protected String doInBackground(String... objects) {
+        System.out.println("Buscar os dados..");
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        String r = new String();
+        try {
+
+            if(objects[0] != null){
+                String nomeLivroCodificado = URLEncoder.encode(objects[0], "UTF-8");
+                url = new URL("http://10.0.2.2:80//Rest/rest.php?nome=" + nomeLivroCodificado);
+            }
+            else{
+                url = new URL("http://10.0.2.2:80//Rest/rest.php");
+            }
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+
+                r += current;
+
+            }
+        } catch (Exception e) {
+            Log.d("Erro: ", e.toString());
+
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return r;
+
+    }
+
+
+    @Override
+    protected void onPostExecute(String o) {
+        super.onPostExecute(o);
+
+
+        ma.exibirListagem(o);
+    }
+}
---
+++ app/src/main/java/com/example/cnec/listarlivros/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.cnec.listarlivros;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ListarLivros(View v){
+        ConsultarServico c = new ConsultarServico(this);
+
+        EditText entrada = findViewById(R.id.entradaDeDados);
+
+        c.execute(entrada.getText().toString());
+    }
+    public void exibirListagem(String o) {
+        try {
+            JSONArray jsonArray = new JSONArray(o);
+            TableLayout tl = findViewById(R.id.tl);
+            tl.removeAllViews();
+
+            TextView twId = new TextView(this);
+            twId.setText("Id");
+            twId.setTextSize(30);
+
+            TextView twNome = new TextView(this);
+            twNome.setText("Ano");
+            twNome.setTextSize(30);
+
+            TextView twAno = new TextView(this);
+            twAno.setText("Nome");
+            twAno.setTextSize(30);
+
+            TableRow tr = new TableRow(this);
+
+            tr.addView(twId);
+            tr.addView(twAno);
+            tr.addView(twNome);
+            tl.addView(tr);
+
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject JSONLivro = jsonArray.getJSONObject(i);
+
+                twId = new TextView(this);
+                twId.setText(JSONLivro.get("id").toString());
+                twId.setTextSize(30);
+
+                twNome = new TextView(this);
+                twNome.setText(JSONLivro.get("nome").toString());
+                twNome.setTextSize(30);
+
+                twAno = new TextView(this);
+                twAno.setText(JSONLivro.get("ano").toString());
+                twAno.setTextSize(30);
+
+                tr = new TableRow(this);
+
+                tr.addView(twId);
+                tr.addView(twAno);
+                tr.addView(twNome);
+                tl.addView(tr);
+//                Log.d("Nome:", JSONLivro.get("nome").toString());
+//                Log.d("Ano:", JSONLivro.get("ano").toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
